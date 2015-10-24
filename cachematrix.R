c7c6ac767362965a6d297117b33bdf0304c3df9c
JVAQUEROM <javaquemarti@hotmail.com>
2015-10-24 02:16:47 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,19 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This two functions are related to matrices and their inverse.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special matrix out of a normal matrix
+## It is actually a list with the matrix and for functions to get
+## and set the value of the matrix, and get and set the value of
+## the inverse of the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
   inverse <- NULL
+  ## We don't want the inverse to be calculated and stored
+  ## as this might take a longer calculation time
   set <- function(y) {
     x <<- y
     inverse <<- NULL
+    ## The inverse must be turned to NULL every time
+    ## the matrix changes.
   }
   get <- function() x
   setInverse <- function(inver) inverse <<- inver
@@ -18,20 +24,22 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
+## CacheSolve retrieves the stored value of the inverse of the matrix
+## but if it doesn't exist, it will calculate it.
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  if(nrow(x$get()) == ncol(x$get())){
+  if(nrow(x$get()) == ncol(x$get())){ # We only calculate inverses when the matrix is squared
     m <- x$getInverse()
-    if(!is.null(m)) {
+    if(!is.null(m)) { ## We'll get a mesage if the matrix is retrieved from cache
       message("getting cached data")
-      return(m)
+      return(m) # we pass m to the function and exit it directly
     }
     data <- x$get()
     m <- solve(data)
     x$setInverse(m)
   }
-  else m <- NULL
+  else m <- NULL ## If the matrix is not squared we won't calculate the inverse as it doesn't exist
   m
 }
